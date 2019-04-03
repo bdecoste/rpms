17b3faf81a333034ea7913481b8f7529d72425c8
William DeCoste <bdecoste@gmail.com>
2019-04-03 12:44:00 -0700

    update

--- istio-proxy/fetch.sh
+++ istio-proxy/fetch.sh
@@ -157,7 +157,7 @@ function replace_python() {
 function update_envoy(){
   pushd ${FETCH_DIR}/istio-proxy/proxy
     sed -i "s|925810d00b0d3095a8e67fd4e04e0f597ed188bb|8912fa36acdf4367d37998d98cead376762d2b49|g" WORKSPACE
-#    sed -i "s|26d1f14e881455546cf0e222ec92a8e1e5f65cb2c5761d63c66598b39cd9c47d|4a87094ef0a113a66baa5841cc19a0eb8524e2078cf9b495ce3f950705c63905|g" WORKSPACE
+    sed -i "s|26d1f14e881455546cf0e222ec92a8e1e5f65cb2c5761d63c66598b39cd9c47d|4a87094ef0a113a66baa5841cc19a0eb8524e2078cf9b495ce3f950705c63905|g" WORKSPACE
   popd
 }
 
